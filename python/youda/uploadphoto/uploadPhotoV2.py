47d4e288da91319117c4d069ea78d74db7455c4d
unknown <350810375@qq.com>
2020-02-03 15:29:46 +0800

    sync

---
+++ python/youda/uploadphoto/uploadPhotoV2.py
@@ -0,0 +1,131 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+
+import time
+import re
+import shutil
+import os
+
+browser = webdriver.Chrome(executable_path = "chromedriver.exe")
+#browser = webdriver.Firefox(executable_path = "./geckodriver")
+
+url = 'https://auokm1.auo.com/ContractSafetyMgt/Webform/Vendor/LoginVendor.aspx'
+login = '9132050773940103XT'
+passssword = '65071281'
+prefix = 'https://auokm1.auo.com/ContractSafetyMgt/Webform/Training/'
+#current_dir = "/home/zjw/workplace/python/youda/uploadphoto/"
+current_dir = "D:/workplace/git/study/python/youda/uploadphoto/template/"
+template_photo_path = current_dir + 'shwj.xlsx'
+
+browser.get(url)
+browser.find_element_by_id("txtLogInId").send_keys(login)
+browser.find_element_by_id("txtPassword").send_keys(passssword)
+
+listUrl = []
+
+def selectAll():
+   
+    selectElements = browser.find_elements_by_tag_name("select")
+
+    for selectElement in selectElements:
+        if('GvPageSizeList' in selectElement.get_attribute('id')):
+            Select(selectElement).select_by_visible_text("All")
+            break
+        
+       
+
+def findNeedUploadUrl():
+    checkedElemets = browser.find_elements_by_css_selector("#ctl00_contentBody_Form_c_SignUpForm1_AddBatch_c_AddBatchWorkers1_Trainee_c_ExtendGridView1 a")
+
+    for element in checkedElemets:
+        if(element.text != "上传审核文件"):
+            continue
+        
+        isUpload = element.find_element_by_xpath('../preceding-sibling::td[2]').text.strip()
+        if(isUpload == "Y"):
+            continue
+
+        name = element.find_element_by_xpath('../preceding-sibling::td[6]').text.strip()
+        new_url = element.get_attribute('href')
+        listUrl.append(new_url)
+        return new_url, name
+
+    return "", ""
+
+def upload(name, photoUrl):
+    current_handle = browser.current_window_handle
+
+    new_path = current_dir + name + '.xlsx'
+       
+    js="window.open('%s')" % photoUrl
+    browser.execute_script(js)
+
+    
+
+    while(len(browser.window_handles) == 1):
+        time.sleep(1)
+        
+    print(len(browser.window_handles))
+    browser.switch_to.window(browser.window_handles[1])
+    time.sleep(1)
+        
+    WebDriverWait(browser, 10).until(expected_conditions.presence_of_element_located((By.ID, "ctl00_contentBody_FileUpload1")))
+
+
+    browser.find_element_by_id("ctl00_contentBody_FileUpload1").send_keys(new_path)
+    browser.find_element_by_id('ctl00_contentBody_Button1').click()
+    time.sleep(1)
+    browser.find_element_by_id('ctl00_contentBody_BtnDone').click()
+    time.sleep(1)
+    dig_alert = browser.switch_to.alert
+    time.sleep(1)
+    dig_alert.accept()
+    time.sleep(1)
+    #os.remove(new_path)
+    browser.switch_to.window(current_handle)
+    time.sleep(1)
+
+
+def start():
+    url = input("请输入上传地址： ")
+    while(not url):
+        url = input("请输入上传地址")
+
+    #url ='https://auokm1.auo.com/ContractSafetyMgt/Webform/Training/SignUpClass/SignUpQuery.aspx?ClassId=82E36AD6A91D9021'
+    browser.get(url)
+
+    selectAll()
+
+    
+    
+    #browser.find_element_by_xpath
+    elements = browser.find_elements_by_css_selector("#ctl00_contentBody_Query_c_ClassWorkers1_Trainee_c_ExtendGridView1 a")
+    for element in elements:
+        if(element.text != '未上传'):
+            continue
+
+        element.find_element_by_xpath('../preceding-sibling::td[1]/a').click()
+        #findCheckElements()
+        break
+       
+    while(True):
+        photoUrl, name = findNeedUploadUrl()
+        if(photoUrl == ""):
+            break
+
+        upload(name, photoUrl)
+        WebDriverWait(browser, 100).until_not(expected_conditions.presence_of_element_located((By.CLASS_NAME, "blockUI")))
+
+    
+
+    print('照片全部上传玩')
+    start()
+    
+       
+start()
+
+
---
+++ python/动手深度学习/d2lzh_pytorch/utils.py
@@ -0,0 +1,132 @@
+from IPython import display
+from matplotlib import pyplot as plt
+import torch
+import torchvision
+import sys
+from torch import nn
+
+# ###################### 3.2 ############################
+def use_svg_display():
+    # 用矢量图显示
+    display.set_matplotlib_formats('svg')
+
+def set_figsize(figsize=(3.5, 2.5)):
+    use_svg_display()
+    # 设置图的尺寸
+    plt.rcParams['figure.figsize'] = figsize
+
+# ######################3##### 3.5 #############################
+def get_fashion_mnist_labels(labels):
+    text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
+                   'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+    return [text_labels[int(i)] for i in labels]
+
+def show_fashion_mnist(images, labels):
+    use_svg_display()
+    # 这里的_表示我们忽略（不使用）的变量
+    _, figs = plt.subplots(1, len(images), figsize=(12, 12))
+    for f, img, lbl in zip(figs, images, labels):
+        f.imshow(img.view((28, 28)).numpy())
+        f.set_title(lbl)
+        f.axes.get_xaxis().set_visible(False)
+        f.axes.get_yaxis().set_visible(False)
+    # plt.show()
+
+# ######################3##### 3.6 #############################
+def evaluate_accuracy(data_iter, net):
+    acc_sum, n = 0.0, 0
+    for x,y in data_iter:
+        
+        acc_sum += (net(x).argmax(axis=1) == y).sum().item()
+        n += y.shape[0]
+    
+        #print(n)
+        #print(y.shape)
+    return acc_sum / n
+    
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, batch_size, params = None, lr = None, optimizer = None):
+    for epoch in range(num_epochs):
+        train_l_sum, train_acc_sum, n = 0.0, 0.0, 0
+
+        for x, y in train_iter:
+            y_hat = net(x)
+            l = loss(y_hat, y).sum()
+
+            if optimizer is not None:
+                optimizer.zero_grad()
+            elif params is not None and params[0].grad is not None:
+                for param in params:
+                    param.grad.data.zero_()
+
+            l.backward()
+
+            if optimizer is None:
+                sgd(params, lr, batch_size)
+            else:
+                optimizer.step()
+
+            train_l_sum += l.item()
+            train_acc_sum += (y_hat.argmax(dim=1) == y).sum().item()
+            n += y.shape[0]
+        
+        test_acc = evaluate_accuracy(test_iter, net)
+        
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f'
+              % (epoch + 1, train_l_sum / n, train_acc_sum / n, test_acc))
+
+
+# ########################### 3.7 #####################################3
+class FlattenLayer(torch.nn.Module):
+    def __init__(self):
+        super(FlattenLayer, self).__init__()
+    def forward(self, x): # x shape: (batch, *, *, ...)
+        return x.view(x.shape[0], -1)
+
+# ########################### 3.11 ###############################
+def semilogy(x_vals, y_vals, x_label, y_label, x2_vals=None, y2_vals=None,
+             legend=None, figsize=(3.5, 2.5)):
+    set_figsize(figsize)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.semilogy(x_vals, y_vals)
+    if x2_vals and y2_vals:
+        plt.semilogy(x2_vals, y2_vals, linestyle=':')
+        plt.legend(legend)
+    # plt.show()
+    
+# ########################## 5.6 #########################3
+def load_data_fashion_mnist(batch_size, resize=None, root='../Datasets/FashionMNIST'):
+    """Download the fashion mnist dataset and then load into memory."""
+    trans = []
+    if resize:
+        trans.append(torchvision.transforms.Resize(size=resize))
+    trans.append(torchvision.transforms.ToTensor())
+    
+    transform = torchvision.transforms.Compose(trans)
+    mnist_train = torchvision.datasets.FashionMNIST(root=root, train=True, download=True, transform=transform)
+    mnist_test = torchvision.datasets.FashionMNIST(root=root, train=False, download=True, transform=transform)
+    if sys.platform.startswith('win'):
+        num_workers = 0  # 0表示不用额外的进程来加速读取数据
+    else:
+        num_workers = 4
+    train_iter = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    test_iter = torch.utils.data.DataLoader(mnist_test, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    return train_iter, test_iter
+
+
+def sgd(params, lr, batch_size):
+    # 为了和原书保持一致，这里除以了batch_size，但是应该是不用除的，因为一般用PyTorch计算loss时就默认已经
+    # 沿batch维求了平均了。
+    for param in params:
+        param.data -= lr * param.grad / batch_size # 注意这里更改param时用的param.data
+
+
+def corr2d(X, K):
+    h, w = K.shape
+    Y = torch.zeros(X.shape[0] - h + 1, X.shape[1] - w + 1)
+    for i in range(Y.shape[0]):
+        for j in range(Y.shape[1]):
+            Y[i, j] = (X[i: i + h, j : j + w] * K).sum()
+
+    return Y
